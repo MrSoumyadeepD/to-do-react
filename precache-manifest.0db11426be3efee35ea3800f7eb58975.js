a98862cd8e99c2b3718a8bfa917cfc85e1c64609
Soumyadeep D <soumyajitghoshh@yahoo.com>
2020-05-01 04:07:47 +0530

    Updates

---
+++ precache-manifest.0db11426be3efee35ea3800f7eb58975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ccc9d381588d762fd3398c17f7f917",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "b79c5115f3fd5c10d049",
+    "url": "/to-do-react/static/css/main.af161acb.chunk.css"
+  },
+  {
+    "revision": "d684d9d1d837a14e53b0",
+    "url": "/to-do-react/static/js/2.a96a0104.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/to-do-react/static/js/2.a96a0104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79c5115f3fd5c10d049",
+    "url": "/to-do-react/static/js/main.b4456d0e.chunk.js"
+  },
+  {
+    "revision": "b5a9a06d54b4097d393d",
+    "url": "/to-do-react/static/js/runtime-main.cbc4a485.js"
+  },
+  {
+    "revision": "b3470f6438248a4b2cfb493a930dfd0c",
+    "url": "/to-do-react/static/media/ds.b3470f64.png"
+  }
+]);
